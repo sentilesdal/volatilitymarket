f9b51a904153e91da2dfb9a8358342400ae71308
Matthew Brown <mattfdbrown@gmail.com>
2022-11-05 04:42:35 -0700

    add rainbowkit ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
 import React from "react";
 import "./App.css";
 
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+export const YourApp = () => {};
+
+const addresses = {
+  TicketManager: "0x5a3355f3D503bC6f6F1812357d12cAA9c2e14bcA",
+};
+
 function App() {
   return (
-    <div className="App">
+    <div className="App h-full">
       <header className="App-header">
-        <div className="hero min-h-screen bg-base-200">
-          <div className="hero-content text-center">
-            <div className="max-w-md">
-              <h1 className="text-5xl font-bold">Hello there</h1>
-              <p className="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
-              <button className="btn btn-primary">Get Started</button>
-            </div>
-          </div>
+        <div className="flex flex-col hero min-h-screen bg-base-200 h-full">
+          <Header />
+          <Body />
         </div>
       </header>
     </div>
@@ -24,3 +23,58 @@ function App() {
 }
 
 export default App;
+function Header() {
+  return (
+    <div className="flex justify-between p-2 w-full">
+      <h1 className="text-5xl font-bold">Volatility Protocol</h1>
+      {/* <button className="btn btn-primary">Connect Wallet</button> */}
+      <ConnectButton label="Connect" />
+    </div>
+  );
+}
+
+function Body() {
+  return (
+    <div className="flex justify-around w-full">
+      <div className="flex align-middle space-x-4">
+        <button className="btn btn-primary">Create new ticket</button>
+        <div className="h-full">
+          <input
+            type="radio"
+            name="radio-2"
+            className="radio radio-primary"
+            checked
+          />
+        </div>
+        <div className="h-full">
+          <input type="radio" name="radio-2" className="radio radio-primary" />
+        </div>
+      </div>
+      <div className="flex flex-col h-full text-center space-y-2">
+        <Ticket id="1" />
+        <Ticket id="2" />
+        <Ticket id="3" />
+        <Ticket id="4" />
+      </div>
+    </div>
+  );
+}
+
+interface TicketProps {
+  id: string;
+}
+
+function Ticket(props: TicketProps) {
+  const { id } = props;
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title">{`Ticket ${id}`}</h2>
+        <p></p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Verify</button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [chain.goerli, chain.polygon, chain.optimism, chain.arbitrum],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider
+        chains={chains}
+        theme={darkTheme({
+          accentColor: "hsl(var(--p) / var(--tw-bg-opacity))",
+        })}
+        modalSize="compact"
+      >
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
   </React.StrictMode>
 );
 
