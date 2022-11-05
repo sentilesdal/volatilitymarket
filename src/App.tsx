5ec3e9c26856a0697c00f9adc2293903de65e359
Matthew Brown <mattfdbrown@gmail.com>
2022-11-05 02:22:04 -0700

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <div className="hero min-h-screen bg-base-200">
+          <div className="hero-content text-center">
+            <div className="max-w-md">
+              <h1 className="text-5xl font-bold">Hello there</h1>
+              <p className="py-6">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
