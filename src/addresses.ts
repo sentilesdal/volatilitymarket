58edf46584c3767275c4dadbbc478f5e3208c690
Shiv <shivg56@gmail.com>
2022-11-06 03:27:27 -0800

    changing contract

--- src/addresses.ts
+++ src/addresses.ts
@@ -1,4 +1,4 @@
 export const addresses = {
-  TicketManager: "0x43cf397b9d671b9b672df7c765bcaf1a28baf001",
+  TicketManager: "0xdF86ddE2d0D22B06b10b4d2E6572e7e95278C288",
   Oracle: "0xA5B9d8a0B0Fa04Ba71BDD68069661ED5C0848884",
 };
